125460e817f2692435d4097414ed56ee233ce0bd
Danny Nguyen <dannynvn@gmail.com>
2023-03-27 10:30:18 -0700

    refactor: removed routes structure to consolidate code into server.js

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const employees = require('./employeeRoutes');
-
-// prefix routes in employeeRoutes with employees
-router.use('/employees', employees);
-
-module.exports = router;
