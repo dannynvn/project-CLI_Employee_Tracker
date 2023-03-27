a1a6368316ad75c031931a9577195d83bad56527
Danny Nguyen <dannynvn@gmail.com>
2023-03-27 10:50:20 -0700

    refactor: fixed and successfully connected to database using dotenv

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 // import and require express and mysql2
 const express = require('express');
 const mysql = require('mysql2');
-const routes = require('/routes');
 const sequelize = require('./config/connection');
 
 
 // declare port and instantiate express app
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 // express middleware encoding
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// initiate routes
-app.use(routes);
 
 // connect to database and server
 sequelize.sync({ force: true }).then(() => {
